47ae186ee5cf00bab2ab62b3e03cae2783807476
cjhdevact <zgadmss@outlook.com>
2024-11-23 20:19:31 +0800

    支持关闭更多程序 - 20241123

--- Src/UsefulControl/My Project/AssemblyInfo.vb
+++ Src/UsefulControl/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.8.24111")> 
-<Assembly: AssemblyFileVersion("1.0.8.24111")> 
+<Assembly: AssemblyVersion("1.0.9.24112")> 
+<Assembly: AssemblyFileVersion("1.0.9.24112")> 
